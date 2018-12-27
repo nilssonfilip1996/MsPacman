ca99e222f078098cf9220790977e2cf7d27fe9d2
Aron Polner <aron.polner@gmail.com>
2018-12-27 16:20:37 +0100

    added a new attribute - direction to the closest pill

--- Ms_Pacman/src/pacman/entries/pacman/ID3PacMan.java
+++ Ms_Pacman/src/pacman/entries/pacman/ID3PacMan.java
@@ -3,8 +3,6 @@ package pacman.entries.pacman;
 import id3.id3Pojo.Node;
 
 import pacman.controllers.Controller;
-import pacman.game.Constants.DM;
-import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
 
@@ -19,20 +17,7 @@ public class ID3PacMan extends Controller<MOVE>{
 	private MOVE pacManMove = MOVE.RIGHT;
 	
 	public MOVE getMove(Game game, long timeDue) {
-		// ID3 is called and after execution returns a move
-		
-		
-		
-//		int current=game.getPacmanCurrentNodeIndex();
-//		
-//		for(GHOST ghost : GHOST.values())
-//			if(game.getGhostEdibleTime(ghost)==0 && game.getGhostLairTime(ghost)==0)
-//				if(game.getShortestPathDistance(current,game.getGhostCurrentNodeIndex(ghost))<DISTANCE_CLOSE) {
-//					isGhostClose = true;
-//				}
-//				else {
-//					isGhostClose = false;
-//				}		
+		// ID3 is called and after execution returns a move	
 	
 		return pacManMove;
 	}
